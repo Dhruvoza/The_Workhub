407005f0bd090b33eb89c5fa270e5cdd2a1b8ec2
Dhruv <dhruvzaoza@gmail.com>
2023-06-23 14:39:00 +0530

    new

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {
+      
+  }
+
+  registerForm = new FormGroup({
+    firstname: new FormControl("A"),
+    lastname: new FormControl(""),
+    email: new FormControl(""),
+    mobile: new FormControl(""),
+    gender: new FormControl(""),
+    pwd: new FormControl(""),
+    rpwd: new FormControl("")
+
+  });
+
+  registerSubmited(){
+    console.log(this.registerForm);
+  }
+}
+
